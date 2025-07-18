46977c709a89ba9bcc4dc6306b5a0037c7e3981e
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-07-19 07:23:24 +0900

    feat: SOPT 데모데이 메타데이터 제목 및 설명 업데이트

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -6,21 +6,21 @@ import ImageSection from './components/ImageSection';
 import IntroduceSection from './components/IntroduceSection';
 
 export const metadata: Metadata = {
-  title: 'SOPT 데모데이 - 행사 개요',
+  title: 'SOPT 데모데이',
   description:
-    '36기 AT SOPT 데모데이 행사 개요 페이지입니다. 다양한 서비스들을 만나보세요.',
+    '36기 AT SOPT 데모데이 공식 웹 페이지입니다. 다양한 서비스들을 만나보세요.',
   openGraph: {
-    title: 'SOPT 데모데이 - 행사 개요',
+    title: 'SOPT 데모데이',
     description:
-      '36기 AT SOPT 데모데이 행사 개요 페이지입니다. 다양한 서비스들을 만나보세요.',
+      '36기 AT SOPT 데모데이 공식 웹 페이지입니다. 다양한 서비스들을 만나보세요.',
     images: ['/assets/og-image.png'],
     url: 'https://demoday-at.sopt.org',
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'SOPT 데모데이 - 행사 개요',
+    title: 'SOPT 데모데이',
     description:
-      '36기 AT SOPT 데모데이 행사 개요 페이지입니다. 다양한 서비스들을 만나보세요.',
+      '36기 AT SOPT 데모데이 공식 웹 페이지입니다. 다양한 서비스들을 만나보세요.',
     images: ['/assets/og-image.png'],
   },
 };
