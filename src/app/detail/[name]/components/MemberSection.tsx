1b006b02676362ba76903e89ac7d7999e45dc71f
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-07-22 17:52:48 +0900

    fix: 반응형 줄바꿈 대응

--- src/app/detail/[name]/components/MemberSection.tsx
+++ src/app/detail/[name]/components/MemberSection.tsx
@@ -8,7 +8,7 @@ export default function MemberSection({ members }: Props) {
       {Object.entries(members).map(([role, names]) => (
         <div className="flex items-center gap-[0.4rem]" key={role}>
           <p className="title3_m_14 w-[7.8rem] text-gray-200">{role}</p>
-          <ul className="flex gap-[0.8rem]">
+          <ul className="flex flex-1 flex-wrap gap-[0.8rem]">
             {names.map((member, idx) => (
               <li key={`${member}-${idx}`} className="body2_r_14 text-gray-200">
                 {member}
